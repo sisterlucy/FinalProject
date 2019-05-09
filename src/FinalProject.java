5823bf75fdb9b9687116b04ed316e5d3b600a502
Unknown <lucy.kasler.2022@student.lhschools.org>
2019-05-09 13:57:25 -0400

    Unable to run code

--- src/FinalProject.java
+++ src/FinalProject.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class FinalProject {
 
     public static void main (String args []){
+       Scanner keyboard;
+       String answer;
+       String yes;
+       String no;
+
+       keyboard=new Scanner(System.in);
+
+        System.out.println("Would you like to play Rock, Paper, Scissors? (yes or no");
+        answer=keyboard.nextLine();
+
+        if(answer=="yes");
+            System.out.println("Okay");
 
 
 
 
-        
+
+
     }
 }
