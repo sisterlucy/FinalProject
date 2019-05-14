358806af4c3976729077a5f34118806494ad1813
Unknown <lucy.kasler.2022@student.lhschools.org>
2019-05-14 13:54:50 -0400

    end of tuesday

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-            
+        System.out.println("Select 1, 2, 3, for Rock, Paper, Scissors");
+        while (!= 0){
+            int rock = 1, paper = 2, scissors = 3;
+            int random = (int)Math.random.();
+
+
            
 
 
