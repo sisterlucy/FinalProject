e7727e209a01255cc473c1c528f48f9fb85d2092
Unknown <lucy.kasler.2022@student.lhschools.org>
2019-05-17 13:38:09 -0400

    End of thursday

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,17 +6,65 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Select 1, 2, 3, for Rock, Paper, Scissors");
-        while (!= 0){
+        Scanner keyboard=new Scanner(System.in);
+        String username= keyboard.nextLine();
+        int randomNumber;
+        int b= keyboard.nextInt();
+
+
+
+
+
+        while (b == b) {
             int rock = 1, paper = 2, scissors = 3;
-            int random = (int)Math.random.();
+            int array;
+            String[][] number = {
+                    {"1", "2", "3"}};
 
 
-           
+            if (rock == b) {
+                if (b == rock) {
+                    System.out.println("Tie");
+                } else if (b == paper) {
+                    System.out.println("Congrats! you win");
+                } else if (b == scissors) {
+                    System.out.println("You lose");
+                } else if (b == paper) {
+                    if (b == rock) {
+                        System.out.println("You lose!");
+                    } else if (b == scissors) {
+                        System.out.println("You win!");
+                    } else if (b == paper) {
+                        System.out.println("tie");
+
+                        if (b == scissors) {
+                            if (b == rock) {
+                                System.out.println("You win!");
+                            } else if (b == scissors) {
+                                System.out.println("tie");
+                            } else if (b == paper) {
+                                System.out.println("You lose!");
+                                System.out.println("play again?");
+                            }
 
 
+                        }
+                    }
+                }
+            }
+            b = 1;
+            System.out.println("do you want to play again? if yes, type 4, if no type 5");
+            int yes = 4, no = 5;
+            int answer;
+            answer = keyboard.nextInt();
 
+            if (answer == yes) {
+                System.out.println("rock, paper, scissors, SHOOT");
 
-
+            }
+            if (answer == no) {
+            System.out.println("okay, i hope you had fun");
         }
-    }
+            }
 
+    }}
